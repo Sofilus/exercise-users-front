d8b7905eccff8258423252565265f74978eda96b
Sofilus <sofia00olsson@gmail.com>
2023-03-07 13:58:59 +0100

    Sending new email to backend

--- main.js
+++ main.js
@@ -8,13 +8,35 @@ function createForm () {
 
     const usernameInput = document.createElement('input');
     usernameInput.placeholder = "Namn"
+    usernameInput.id = "name"
     form.appendChild(usernameInput);
 
     const emailInput = document. createElement('input');
-    emailInput.placeholder = "Email"
+    emailInput.placeholder = "Email";
+    emailInput.id = "email"
     form.appendChild(emailInput);
 
     const addEmailBtn = document.createElement('button');
     addEmailBtn.innerHTML = "Lägg till"
+    addEmailBtn.type = "submit"
     form.appendChild(addEmailBtn);
+
+    addEmailBtn.addEventListener('click', sendValue);
+}
+
+function sendValue(){
+    console.log("hh")
+    let nameInput = document.querySelector('#name');
+    let emailInput = document.querySelector('#email')
+    let name = nameInput.value;
+    let email = emailInput.value;
+    let user = {name: name, email: email}
+
+    fetch('http://localhost:3000/email', {
+   method: "POST",
+   headers: {
+      'Content-Type': 'application/json',
+   },
+   body: JSON.stringify(user)
+}) 
 }
